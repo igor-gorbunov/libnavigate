2b5084244729cbf9a7695c2e864f95e1a0dacbe2
Igor Gorbunov <igor.genius@gmail.com>
2012-09-12 14:27:37 +0400

    Updated documentation and installation instructions. Added versioning.

--- src/config.c
+++ src/config.c
@@ -1,9 +1,28 @@
+/*
+ * config.c - libnavigate configuration methods
+ *
+ * Copyright (C) 2012 I. S. Gorbunov <igor.genius at gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <libnavigate/config.h>
 #include <libnavigate/errors.h>
 
 #include "version.h"
 
-struct navi_config_t
+NAVI_ALIGNED(struct, navi_config_t)
 {
 	unsigned int caps;		// capabilities of the library
 	const char *version;	// version of the library
@@ -21,7 +40,7 @@ static struct navi_config_t g_navi_config_t =
 #endif //  NO_PARSER
 
 	0,
-	LIBNAVIGATE_VERSION
+	LIBNAVIGATE_VERSION_STRING
 };
 
 unsigned int naviconf_get_caps(void)
--- src/version.h
+++ src/version.h
@@ -2,8 +2,12 @@
 #define INCLUDE_navi_version_h
 
 //
-// major.minor.patch[-suffix]
+// major.minor.patch[suffix]
 //
-#define LIBNAVIGATE_VERSION		"0.1.0"
+#define LIBNAVIGATE_VERSION_STRING	"0.1.0a"
+
+#define LIBNAVIGATE_VERSION_MAJOR	0
+#define LIBNAVIGATE_VERSION_MINOR	1
+#define LIBNAVIGATE_VERSION_PATCH	0
 
 #endif // INCLUDE_navi_version_h
