5f488f73f397a51e5c081439269db67d21f4610a
Igor Gorbunov <igor.genius@gmail.com>
2012-10-29 10:16:38 +0400

    Implementation of BWW message parser/generator.

---
+++ include/libnavigate/bww.h
@@ -0,0 +1,67 @@
+/*
+ * bww.h - generator and parser of BWW message
+ *
+ * Copyright (C) 2012 I. S. Gorbunov <igor.genius at gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_navi_bww_h
+#define INCLUDE_navi_bww_h
+
+#include <libnavigate/errors.h>
+#include <libnavigate/sentence.h>
+
+//
+// BWW - Bearing waypoint to waypoint
+// Bearing angle of the line, between the TO and the FROM waypoints,
+// calculated at the FROM waypoint for any two arbitrary waypoints.
+// $--BWW,x.x,T,x.x,M,c--c,c--c*hh<cr><lf>
+//
+struct bww_t
+{
+	// Talker id
+	navi_talkerid_t tid;
+
+	// Bearing, degrees true
+	struct navi_offset_t bearingT;
+
+	// Bearing, degrees magnetic
+	struct navi_offset_t bearingM;
+
+	// TO waypoint ID
+	char to_waypoint[21];
+
+	// FROM waypoint ID
+	char from_waypoint[21];
+};
+
+NAVI_BEGIN_DECL
+
+//
+// Initializes BWW sentence structure with default values
+NAVI_EXTERN(navierr_status_t) navi_init_bww(struct bww_t *msg, navi_talkerid_t tid);
+
+//
+// Creates BWW message
+NAVI_EXTERN(navierr_status_t) navi_create_bww(const struct bww_t *msg, char *buffer,
+	size_t maxsize, size_t *nmwritten);
+
+//
+// Parses BWW message
+NAVI_EXTERN(navierr_status_t) navi_parse_bww(struct bww_t *msg, char *buffer);
+
+NAVI_END_DECL
+
+#endif // INCLUDE_navi_bww_h
